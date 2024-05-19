1da2aa206bcb74ac08066014c09990af0634533b
David Kim <David.Kim@carvana.com>
2024-05-18 22:16:36 -0400

    added predeploy script to remove script hashing, also added shadow

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import r2wc from '@r2wc/react-to-web-component';
 import Header from './components/header/Header';
 
-const wcHeader = r2wc(Header, { props: {text: 'string'} });
+const wcHeader = r2wc(Header, { props: {text: 'string'}});
+const wcHeaderShadow = r2wc(Header, { shadow: "open"});
 
 customElements.define("r2w-header", wcHeader);
+customElements.define("r2wc-header-shadow", wcHeaderShadow);
